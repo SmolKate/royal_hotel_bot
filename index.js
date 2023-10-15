88e479545235dfe7e4ab3b2ad4b4015e6649d3fd
SmolKate <ZdobnovaEA@gmail.com>
2023-10-15 14:47:22 +0300

    feat: buttons

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ const bot = new TelegramApi(token, {polling: true}) // create bot
 
 bot.setMyCommands([
     {command: '/start', description: 'Приветствие'},
-    {command: '/info', description: 'Общая информация'},
+    {command: '/wifi', description: 'Общая информация'},
 ])
+
+
+const welcomeOptions = {
+    reply_markup: {
+        keyboard: [
+            [{ text: 'Интернет', callback_data: '/wifi'}],
+            [{ text: 'Интернет', callback_data: '/wifi'}],
+        ]
+    }
+}
 
 const start = () => {
     bot.on('message', async (msg) => {   // request on message receive
@@ -17,16 +27,23 @@ const start = () => {
     
         if (text === '/start') {
             await bot.sendSticker(chatId, './img/canvas.png')
-            return bot.sendMessage(chatId, `${msg.from.first_name} ${msg.from.last_name}, Вас приветствует бот отеля Royal Hotel!`)
+            return bot.sendMessage(chatId, `${msg.from.first_name} ${msg.from.last_name}, Вас приветствует бот отеля Royal Hotel!`, welcomeOptions)
         }
     
-        if (text === '/info') {
+        if (text === '/wifi') {
             return bot.sendMessage(chatId, `Подключиться к сети Wi-Fi. Cеть: ROYAL HOTEL SPA. Пароль: регистарция по Вашему личному номеру телефона`)
         }
     
         return bot.sendMessage(chatId, 'Простите, я Вас не понял. Попробуйте еще раз.')
 
     })  
+
+    bot.on('callback_query', msg => {
+        const data = msg.data
+        console.log(data)
+        const chatId = msg.message.chat.id
+        return bot.sendMessage(chatId, `${data}`)
+    })
 }
 
 start()
