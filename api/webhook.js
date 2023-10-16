02da6ab05a0a4cf8cd60b215f8cc76f46bca3150
SmolKate <ZdobnovaEA@gmail.com>
2023-10-16 20:02:30 +0300

    express

--- api/webhook.js
+++ api/webhook.js
@@ -1,30 +1,23 @@
-const TelegramApi = require('node-telegram-bot-api')
+// const TelegramApi = require('node-telegram-bot-api')
 
-// const token = '6552090051:AAFf-Paq6kck0vlhLnRSvci0O3d0Ud-5Ihw'
-const token = '6512915316:AAFXYqBVCXPZnV5i7TEcECwHwfOH3n018_Y'
+// // const token = '6552090051:AAFf-Paq6kck0vlhLnRSvci0O3d0Ud-5Ihw'
+// const token = '6512915316:AAFXYqBVCXPZnV5i7TEcECwHwfOH3n018_Y'
 
-module.exports = async (request, response) => {
-    try {
-        const bot = new TelegramApi(token, {polling: true}) // create bot
-        const {body} = request
+// module.exports = async (request, response) => {
+//     try {
+//         const bot = new TelegramApi(token, {polling: true}) // create bot
+//         const {body} = request
 
-        if (body.message) {
-            const {chat: {id}, text} = body.message
+//         if (body.message) {
+//             const {chat: {id}, text} = body.message
 
-            await bot.sendMessage(id, `${text}, Вас приветствует бот отеля Royal Hotel!`)
+//             await bot.sendMessage(id, `${text}, Вас приветствует бот отеля Royal Hotel!`)
 
-        }
+//         }
 
-    } catch (error) {
-        console.log(error)
-    }
+//     } catch (error) {
+//         console.log(error)
+//     }
 
-    response.send('ok')
-}
-module.exports = (request, response) => {
-    response.json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies
-    })
-}
+//     response.send('ok')
+// }
